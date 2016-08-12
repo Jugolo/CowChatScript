f853b38cad87604c53d0a62336dea9fe9f6e065f
Ronnie <rix17172@gmail.com>
2016-08-12 10:20:43 +0200

    Update parser.php

--- parser.php
+++ parser.php
@@ -1,9 +1,26 @@
 <?php
 class Script{
-  counst PARSE_STRING = 0;
-  counst PARSE_FILE = 1;
-  counst PARSE_URL = 2;
+  const PARSE_STRING = 0;
+  const PARSE_FILE = 1;
+  const PARSE_URL = 2;
   public function parse(string $data, int $mode = 0){
+     $context = "";
+     switch($mode){
+       case self::PARSE_STRING:
+         $context = $data;
+       break;
+       case self::PARSE_FILE:
+         if(!file_exsists($data)){
+           throw new Exception("Could not find the file: ".$data);
+         }
 
+         $context = file_get_contents($data);
+       break;
+       case self::PARSE_URL:
+         $context = file_get_contents($data);
+       break;
+       deafult:
+         throw new Exception("Unknown mode");
+     }
   }
 }
